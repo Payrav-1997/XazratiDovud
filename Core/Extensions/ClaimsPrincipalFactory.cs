4264507d695ba3f7ac21cb0365e62a13fd11cca4
Payrav-1997 <62432924+Payrav-1997@users.noreply.github.com>
2021-10-12 19:05:49 +0500

    Added LandigPage for Client

---
+++ Core/Extensions/ClaimsPrincipalFactory.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Extensions
+{
+    public class ClaimsPrincipalFactory : UserClaimsPrincipalFactory<User>
+    {
+        public ClaimsPrincipalFactory(UserManager<User> userManager, 
+            IOptions<IdentityOptions> optionsAccessor) 
+            : base(userManager, optionsAccessor)
+        {
+        }
+    }
+}
