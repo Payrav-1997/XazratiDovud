90efddd3dd2a0c7d92a26dd89cc736b8926b2113
Payrav-1997 <payrav-343@mail.ru>
2021-11-29 20:49:28 +0500

    Added SendMethod

--- Services/Mail/EmailService.cs
+++ Services/Mail/EmailService.cs
@@ -1,6 +1,7 @@
 ﻿
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
+using Services.Mail.Models;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Mail;
@@ -10,8 +11,26 @@ namespace Services.Mail
 {
     public class EmailService
     {
-        public EmailService(IOptions<ConnectionOptions> options)
+        private readonly ConnectionOptions _optionsAccessor;
+        private readonly SmtpClient _smtp;
+
+        public EmailService(IOptions<ConnectionOptions> optionsAccessor)
         {
+            this._optionsAccessor = optionsAccessor.Value;
+            _smtp = new SmtpClient();
+        }
+
+
+        public async Task Send(string to, string subject, string body)
+        {
+            var mail = new MailMessage();
+            mail.From = new MailAddress(_optionsAccessor.UserName);
+            mail.To.Add(new MailAddress(to));
+            mail.Subject = subject;
+            mail.IsBodyHtml = true;
+            mail.Body = body;
+            _smtp.Host = _optionsAccessor.Host;
+            _smtp.EnableSsl = _optionsAccessor.UseSsl;
         }
     }
 }
